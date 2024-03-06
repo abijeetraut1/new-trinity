b8e34c9da7beade24bf8ccfe787addb0e5a876a8
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2024-03-07 01:12:18 +0545

    changes on home page

--- public/js/forall.js
+++ public/js/forall.js
@@ -1,45 +1,10 @@
-document.querySelector('.bi-list').addEventListener("click", el => {
-    if (document.querySelector('.hidden-part').classList.contains('isHidden')) {
-        document.querySelector('.hidden-part').classList.toggle('isHidden');
-        document.querySelector('.hidden-part').classList.remove('adder');
-        document.querySelector('.hidden-part').classList.toggle('active');
-        // alert('active');
-        gsap.fromTo('.active', {
-            display: 'block',
-            height: '0px'
-        }, {
-            height: '170px',
-            duration: .3,
-            ease: Power0.easeNone
-        });
-    } else if (document.querySelector('.hidden-part').classList.contains('active')) {
-        // alert('hah')
-        document.querySelector('.hidden-part').classList.add('adder');
-        document.querySelector('.hidden-part').classList.toggle('isHidden');
-        document.querySelector('.hidden-part').classList.toggle('active');
-        gsap.fromTo('.isHidden', {
-            display: 'block',
-            height: '170px'
-        }, {
-            height: '0px',
-            duration: .3,
-            ease: Power0.easeNone
-        });
-    }
-});
-
-// $('#logout')[0].addEventListener('click', async el => {
-// function formatNumber(num) {
-//     return num.toFixed(2);
-// }
-
 async function logout (){
     const logout = await axios({
         method: 'GET',
         url: '/api/v1/user/logout',
-
     })
     if (logout.data.status === "success") {
+        console.log("loggin out")
         window.location.assign('/')
     }
 }
@@ -59,12 +24,6 @@ if (window.location.pathname.split('/')[1] === "product" && window.location.path
 } else if (window.location.pathname.split('/')[1] === "product") {
     $('.bi-list, .bi-bag').css('font-size', '2.3rem');
     $('.add-cart, .ham-menu').css('overflow', 'hidden');
-}
-
-if($('#loged-in')[0]){
-    document.querySelector('#loged-in').addEventListener('click', el => {
-        $('#logout')[0].classList.toggle('logout-active')
-    })
 }
 
 if(window.innerWidth >= 640){
