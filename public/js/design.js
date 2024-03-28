1cf1b5c11e4481eec4d1868c5a6e14ae86735c3f
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2024-03-28 21:52:18 +0545

    design.js changed

--- public/js/design.js
+++ public/js/design.js
@@ -646,6 +646,41 @@ if (!($("#for-admin-only")[0])) {
 }
 
 var divToCapture = document.getElementById('product');
+var detailForm = document.getElementById('sellThisAdmin');
+
+detailForm.addEventListener("click", () => {
+    html2canvas(divToCapture).then(function (canvas) {
+        html2canvas(document.getElementById("product")).then(function (canvas) {
+            var img = canvas.toDataURL('image/png');
+            sessionStorage.setItem(`designed${$('.btn-toggle')[0].innerText}View`, img); // jun view set bhako xa tai ko image linxa
+
+            if (!($('.btn-toggle')[0].innerText === 'Front')) {
+                $('.front').css("display", "block");
+                $('.back').css("display", "none");
+                productImageDisplay.style.backgroundImage = `url("product_img/${sessionStorage.getItem('selected_type')}_front.png")`;
+
+                html2canvas(divToCapture).then(function (canvass) {
+                    html2canvas(document.getElementById("product")).then(function (canvass) {
+                        var frontImg = canvass.toDataURL('image/png');
+                        sessionStorage.setItem('designedFrontView', frontImg); // save the tshirt front view in the local storage
+                    });
+                });
+            }
+
+            if (!($('.btn-toggle')[0].innerText === 'Back')) {
+                $('.front').css("display", "none");
+                $('.back').css("display", "block");
+                productImageDisplay.style.backgroundImage = `url("product_img/${sessionStorage.getItem('selected_type')}_back.png")`;
+                html2canvas(divToCapture).then(function (canvass) {
+                    html2canvas(document.getElementById("product")).then(function (canvass) {
+                        var backImg = canvass.toDataURL('image/png');
+                        sessionStorage.setItem('designedBackView', backImg); // save the tshirt front view in the local storage
+                    });
+                });
+            }
+        });
+    });
+})
 
 // for admins to design tshirt
 if ($("#for-admin-only")[0]) {
@@ -667,38 +702,7 @@ if ($("#for-admin-only")[0]) {
             tags: tags,
         }
 
-        
-        html2canvas(divToCapture).then(function (canvas) {
-            html2canvas(document.getElementById("product")).then(function (canvas) {
-                var img = canvas.toDataURL('image/png');
-                sessionStorage.setItem(`designed${$('.btn-toggle')[0].innerText}View`, img); // jun view set bhako xa tai ko image linxa
 
-                if (!($('.btn-toggle')[0].innerText === 'Front')) {
-                    $('.front').css("display", "block");
-                    $('.back').css("display", "none");
-                    productImageDisplay.style.backgroundImage = `url("product_img/${sessionStorage.getItem('selected_type')}_front.png")`;
-
-                    html2canvas(divToCapture).then(function (canvass) {
-                        html2canvas(document.getElementById("product")).then(function (canvass) {
-                            var frontImg = canvass.toDataURL('image/png');
-                            sessionStorage.setItem('designedFrontView', frontImg); // save the tshirt front view in the local storage
-                        });
-                    });
-                }
-
-                if (!($('.btn-toggle')[0].innerText === 'Back')) {
-                    $('.front').css("display", "none");
-                    $('.back').css("display", "block");
-                    productImageDisplay.style.backgroundImage = `url("product_img/${sessionStorage.getItem('selected_type')}_back.png")`;
-                    html2canvas(divToCapture).then(function (canvass) {
-                        html2canvas(document.getElementById("product")).then(function (canvass) {
-                            var backImg = canvass.toDataURL('image/png');
-                            sessionStorage.setItem('designedBackView', backImg); // save the tshirt front view in the local storage
-                        });
-                    });
-                }
-            });
-        });
 
         parentElement.childNodes.forEach((el, i) => {
             if (i == 0) console.log(el);
@@ -720,35 +724,30 @@ if ($("#for-admin-only")[0]) {
             }
         })
 
+        if (sessionStorage.designedBackView && sessionStorage.designedFrontView) {
+            data.frontImage = sessionStorage.getItem('designedFrontView');
+            data.backImage = sessionStorage.getItem('designedBackView');
 
-        setTimeout(async () => {
-            if (sessionStorage.designedBackView && sessionStorage.designedFrontView) {
-                data.frontImage = sessionStorage.getItem('designedFrontView');
-                data.backImage = sessionStorage.getItem('designedBackView');
-                
-                console.log(data)
+            console.log(data)
 
-                const postData = await axios({
-                    method: "POST",
-                    url: "/api/v1/product/design/upload",
-                    headers: {
-                        "Content-Type": "multipart/form-data"
-                    },
-                    data: data
-                });
-    
-                console.log(postData)
-    
-                if (postData.data.status = "success") {
-                    sessionStorage.removeItem('image');
-    
-                    // window.location.assign(`${window.origin}/product/${slug}`)
-                } else {
-                    alert('plese fill up the information carefully');
-                }
+            const postData = await axios({
+                method: "POST",
+                url: "/api/v1/product/design/upload",
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                },
+                data: data
+            });
+
+            console.log(postData)
+
+            if (postData.data.status = "success") {
+                sessionStorage.removeItem('image');
+                window.location.assign(`${window.origin}/product/${slug}`)
+            } else {
+                alert('plese fill up the information carefully');
             }
-
-        }, 5000);
+        }
 
         // console.log(postData);
     })
